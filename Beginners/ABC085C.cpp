d5c8f05c9de3061672d265dc418bc570526b0a0a
UENO Takahiro <uenotakahirooo@gmail.com>
2018-09-23 16:26:20 +0900

    Solved ABC085C.

--- Beginners/ABC085C.cpp
+++ Beginners/ABC085C.cpp
@@ -2,29 +2,21 @@
 using namespace std;
 
 int main() {
-  // 入力
   int N, Y;
   cin >> N >> Y;
 
-  // 回答
-  int num_x, num_y, num_z;
+  int num_x = -1, num_y = -1, num_z = -1;
 
   for(int x = 0; x <= N; x++){
-    for(int y = 0; y <= N; y++){
-      for(int z = 0; z <= N; z++){
-        int total = 10000*x + 5000*y + 1000*z;
-        if(x + y + z == N && total == Y){
-          num_x = x;
-          num_y = y;
-          num_z = z;
-        } else {
-          num_x = -1;
-          num_y = -1;
-          num_z = -1;
-        }
+    for(int y = 0; y + x <= N; y++){
+      int z = N - x - y;
+      int total = 10000*x + 5000*y + 1000*z;
+      if(total == Y){
+        num_x = x;
+        num_y = y;
+        num_z = z;
       }
     }
   }
-
   cout << num_x << " " << num_y << " " << num_z << endl;
 }
