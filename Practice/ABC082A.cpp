fa5ec715a9bb7375b9ff1264321b2a3ac1b452f9
UENO Takahiro <uenotakahirooo@gmail.com>
2018-09-29 17:07:53 +0900

    Add ABC082A

--- Practice/ABC082A.cpp
+++ Practice/ABC082A.cpp
@@ -5,5 +5,15 @@ int main(){
   int a, b;
   cin >> a >> b;
 
-  
+  int res;
+
+  if(a%2 == 1 && b%2 == 0){
+    res = (a + b) / 2 + 1;
+  } else if(a%2 == 0 && b%2 == 1) {
+    res = (a + b) / 2 + 1;
+  } else {
+    res = (a + b) /2;
+  }
+
+  cout << res << endl;
 }
