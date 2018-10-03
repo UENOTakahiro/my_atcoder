7bdddc0d60fbba9acd47095011864c0c4e33d1b5
UENO Takahiro <uenotakahirooo@gmail.com>
2018-10-03 21:41:44 +0900

    Solve ABC076B

--- Practice/ABC076B.cpp
+++ Practice/ABC076B.cpp
@@ -5,5 +5,14 @@ int main(){
   int N, K;
   cin >> N >> K;
 
-  
+  int point = 1;
+  for(int i=0;i<N;i++){
+    if(2 * point > point + K){
+      point += K;
+    } else {
+      point *= 2;
+    }
+  }
+
+  cout << point << endl;
 }
